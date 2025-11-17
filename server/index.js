7281d43dc2e69029265f1e70c2e1d1e8f364c21b
Imtiaz4530 <apual4016@gmail.com>
2025-11-17 21:56:59 +0600

    1st - pc - besic server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./db/connectDB");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
